92de1f424b6d1924ac04dc0b6260f81ddba37267
Bruno Andrade <bruno@artsman.dev>
2023-12-15 13:27:24 +0000

    feat: base project files

---
+++ jpa-cyber-commerce/src/main/java/dev/artsman/cybercommerce/Application.java
@@ -0,0 +1,16 @@
+package dev.artsman.cybercommerce;
+
+/**
+ * Simple Application class
+ */
+class Application {
+	private final String name;
+
+	Application(String name) {
+		this.name = name;
+	}
+
+	String name() {
+		return this.name;
+	}
+}
